f60744d545edb324669c6eff86704d41de95e5f4
William Keith Pendleton <wkp23@byu.edu>
2024-10-11 19:16:53 -0600

    Initial Commit

---
+++ High Performance Planner/High_Performance_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  High_Performance_PlannerApp.swift
+//  High Performance Planner
+//
+//  Created by William Pendleton on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct High_Performance_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
