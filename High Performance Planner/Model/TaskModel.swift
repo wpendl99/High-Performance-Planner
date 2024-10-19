dbbfcb6e561874da57444c7599e0aee76253d39f
William Keith Pendleton <wkp23@byu.edu>
2024-10-19 01:55:10 -0600

    add(OnMyMindManager) and update(TaskModel) to make Tasks Equatable

--- High Performance Planner/Model/TaskModel.swift
+++ High Performance Planner/Model/TaskModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol Task: Identifiable, Codable {
+protocol Task: Identifiable, Codable, Equatable {
     var id: UUID { get set }
     var description: String { get set }
     var notes: String? { get set }
